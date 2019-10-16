e51efbb60f577ffe3dd3e4d7545e5a7c93be03cf
Chenyp <863762649@qq.com>
2019-10-16 23:04:24 +0800

    '提交'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tab-bar-item" @click="btnClick">
+    <div v-if="isAction">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-action"></slot>
+    </div>
+
+    <div :class="{active:!isAction}">
+      <slot name="item-text"></slot>
+    </div>
+
+  </div>
+<!--  <div class="tab-bar-item">-->
+<!--    <img src="../../assets/img/tabbar/home.svg"  alt="">-->
+<!--    <div>首页</div>-->
+<!--  </div>-->
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props:{
+      path:String
+    },
+    data(){
+      return{
+        // isAction:true
+      }
+    },
+    computed:{
+      isAction(){
+        return this.$route.path.indexOf(this.path) ==-1
+      }
+    },
+    methods:{
+      btnClick(){
+        try{
+          this.$router.push(this.path).catch(err => { console.log(err) })
+        }catch (err) {
+
+        }
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+  }
+  .active{
+    color: red;
+  }
+</style>
